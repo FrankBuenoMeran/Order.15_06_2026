4f20ad44bc002fedfafcd0b86bc15afb192a669a
USER <Frank.buenomeran88@gmail.com>
2026-06-19 15:16:43 -0400

    Add Post and Get for countries

---
+++ Orders/Orders.Backend2/Controllers/ContriesController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using Orders.Backend2.Data;
+using Orders.Shared.Entities;
+using System.Threading.Tasks;
+
+namespace Orders.Backend2.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ContriesController : ControllerBase
+{
+    private readonly DataContext _context;
+
+    public ContriesController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAsync()
+    {
+        return Ok(await _context.Countries.ToListAsync());
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Post(Country country)
+    {
+        _context.Countries.Add(country);
+        await _context.SaveChangesAsync();
+        return Ok(country);
+    }
+}
